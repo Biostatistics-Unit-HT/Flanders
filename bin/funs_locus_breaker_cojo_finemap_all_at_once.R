14bb67fba736d1f703b7b193beb3652d38bfb44d
arianna.landini <arianna.landini@external.fht.org>
2025-05-21 11:47:13 +0200

    Re-adding this function to perform coloc with pre-computed lABFs

--- bin/funs_locus_breaker_cojo_finemap_all_at_once.R
+++ bin/funs_locus_breaker_cojo_finemap_all_at_once.R
@@ -160,6 +160,49 @@ prep_susie_ld <- function(
 
 
 
+### hcolo.cojo.ht ###
+hcolo.cojo.ht=function(df1 = conditional.dataset1,
+                       df2 = conditional.dataset2,
+                       p1=1e-4,
+                       p2=1e-4,
+                       p12=1e-5
+                       ){
+  
+  df1 <- df1 %>% dplyr::rename("lABF.df1"="lABF")
+  df2 <- df2 %>% dplyr::rename("lABF.df2"="lABF")
+  
+  p1 <- coloc:::adjust_prior(p1, nrow(df1), "1")
+  p2 <- coloc:::adjust_prior(p2, nrow(df2), "2")
+      
+  merged.df <- merge(df1, df2, by = "snp")
+  p12 <- coloc:::adjust_prior(p12, nrow(merged.df), "12")
+    
+  if(!nrow(merged.df))
+    stop("dataset1 and dataset2 should contain the same snps in the same order, or should contain snp names through which the common snps can be identified")
+      
+  merged.df$internal.sum.lABF <- with(merged.df, lABF.df1 + lABF.df2)
+## add SNP.PP.H4 - post prob that each SNP is THE causal variant for a shared signal
+  my.denom.log.abf <- coloc:::logsum(merged.df$internal.sum.lABF)
+  merged.df$SNP.PP.H4 <- exp(merged.df$internal.sum.lABF - my.denom.log.abf)
+      
+  pp.abf <- coloc:::combine.abf(merged.df$lABF.df1, merged.df$lABF.df2, p1, p2, p12)  
+  common.snps <- nrow(merged.df)
+  results <- c(nsnps=common.snps, pp.abf)
+      
+  colo.res <- list(summary=results, results=merged.df, priors=c(p1=p1,p2=p2,p12=p12))
+  class(colo.res) <- c("coloc_abf", class(colo.res))
+      
+## Save coloc summary        
+  colo.sum <- data.frame(t(colo.res$summary))
+      
+## Save coloc result by SNP
+  colo.full_res <- colo.res$results %>% dplyr::select(snp,lABF.df1,lABF.df2,SNP.PP.H4)
+
+## Organise all in a list ( composed of summary + results)
+  coloc.final <- list(summary=colo.sum, results=colo.full_res)
+  return(coloc.final)
+}
+
 
 
 # Sodbo Sharapov, Human Technopole (c) 2024
