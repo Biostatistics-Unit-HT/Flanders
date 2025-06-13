3d35deb002d7c8ae6c44bbdd5081c5c8a88b91d5
edoardo.giacopuzzi <edoardo.giacopuzzi@fht.org>
2025-06-13 09:28:57 +0200

    different filter logic for coloc exclusion from input or from previous h5ad

--- bin/s09_make_coloc_guide.R
+++ bin/s09_make_coloc_guide.R
@@ -15,10 +15,15 @@ option_list <- list(
               default = NULL,
               help = "Input h5ad file",
               metavar = "character"),
-  make_option(c("-x", "--exclude"),
+  make_option(c("-x", "--exclude_bothsides"),
               type = "character",
               default = NULL,
-              help = "Tab separated file with header study_id, phenotype_id that report traits to exclude",
+              help = "Tab separated file with header study_id, phenotype_id that report traits to exclude when both t1 and t2 are in the list",
+              metavar = "character"),
+  make_option(c("-x", "--exclude_oneside"),
+              type = "character",
+              default = NULL,
+              help = "Tab separated file with header study_id, phenotype_id that report traits to exclude when either t1 or t2 are in the list",
               metavar = "character"),
   make_option(c("-o", "--output_file"),
               type = "character",
@@ -39,17 +44,20 @@ message("Making coloc guide table")
 coloc_input <- flanders::anndata2coloc_input(sce)
 message(nrow(coloc_input), " coloc tests generated")
 
-# Read the exclusion table using read_delim and convert to data.table
-exclude_dt <- readr::read_delim(opt$exclude, delim = "\t", col_names = TRUE)
-exclude_dt <- as.data.table(exclude_dt)
-exclude_dt <- exclude_dt[study_id != "EMPTY"]
+# Make a unique id useful for filtering
+coloc_input[, t1_uniqueid := paste(t1_study_id, t1_phenotype_id, sep = "_")]
+coloc_input[, t2_uniqueid := paste(t2_study_id, t2_phenotype_id, sep = "_")]
 
-# If there are studies to exclude, remove them
-if (nrow(exclude_dt) > 0) {
-  message("Found ", nrow(exclude_dt), " studies to exclude")
-  exclude_dt[, unique_id := paste(study_id, phenotype_id, sep = "_")]
-  coloc_input[, t1_uniqueid := paste(t1_study_id, t1_phenotype_id, sep = "_")]
-  coloc_input[, t2_uniqueid := paste(t2_study_id, t2_phenotype_id, sep = "_")]
+# Read the exclusion tables and convert to data.table
+exclude_2sides <- readr::read_delim(opt$exclude_bothsides, delim = "\t", col_names = TRUE)
+exclude_2sides <- as.data.table(exclude_2sides)
+exclude_2sides <- exclude_2sides[study_id != "EMPTY"]
+
+# Exclude studies where both t1 and t2 are in the exclusion list
+if (nrow(exclude_2sides) > 0) {
+  message("Found ", nrow(exclude_2sides), " entries in both side exclusion")
+  exclude_2sides[, unique_id := paste(study_id, phenotype_id, sep = "_")]
+
   coloc_input[, t1_excluded := t1_uniqueid %in% exclude_dt$unique_id]
   coloc_input[, t2_excluded := t2_uniqueid %in% exclude_dt$unique_id]
   coloc_input <- coloc_input[!(coloc_input$t1_excluded & coloc_input$t2_excluded)]
@@ -57,5 +65,26 @@ if (nrow(exclude_dt) > 0) {
   message(nrow(coloc_input), " coloc tests remaining after exclusion")
 }
 
+if (!is.null(opt$exclude_oneside)) {
+  exclude_1side <- readr::read_delim(opt$exclude_oneside, delim = "\t", col_names = TRUE)
+  exclude_1side <- as.data.table(exclude_1side)
+  
+  message("Found ", nrow(exclude_1side), " entries in one side exclusion")
+  exclude_1side[, unique_id := paste(study_id, phenotype_id, sep = "_")]
+
+  coloc_input[, t1_excluded := t1_uniqueid %in% exclude_1side$unique_id]
+  coloc_input[, t2_excluded := t2_uniqueid %in% exclude_1side$unique_id]
+  coloc_input <- coloc_input[!(coloc_input$t1_excluded | coloc_input$t2_excluded)]
+
+  message(nrow(coloc_input), " coloc tests remaining after exclusion"s)
+}
+
+# Remove accessory columns
+coloc_input[, t1_uniqueid := NULL]
+coloc_input[, t2_uniqueid := NULL]
+coloc_input[, t1_excluded := NULL]
+coloc_input[, t2_excluded := NULL]
+
+# Save coloc guide table
 data.table::fwrite(coloc_input, file = opt$output_file)
 message("Coloc guide table saved to ", opt$output_file)
