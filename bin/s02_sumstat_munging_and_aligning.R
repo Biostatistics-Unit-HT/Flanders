8c3fa83ba12c02c3abadf48057bee374990a9b8f
Arianna Landini <al37@sanger.ac.uk>
2026-03-25 09:23:41 +0000

    Add to dataset.align() the argument specifying bfile_type (plink1|plink2)

--- bin/s02_sumstat_munging_and_aligning.R
+++ bin/s02_sumstat_munging_and_aligning.R
@@ -274,17 +274,8 @@ hg19ToHg38_liftover <- function(
 }
 
 
-### Auto-detect genotype format (pgen vs bed) for plink2 input flag
-get_plink2_input_flag <- function(prefix) {
-  if (file.exists(paste0(prefix, ".pgen"))) {
-    return(paste0("--pfile ", prefix))
-  } else {
-    return(paste0("--bfile ", prefix))
-  }
-}
-
 ### dataset.align function --------
-dataset.align <- function(dataset, bfile) {
+dataset.align <- function(dataset, bfile, bfile_type) {
   
   # Load munged dataset sumstat in .rds format (if necessary)
   if(is.character(dataset)){
@@ -315,8 +306,12 @@ dataset.align <- function(dataset, bfile) {
     message("Allele frequency not found in summary stat - Computing allele frequency from LD reference panel")
     ## Compute allele frequency from LD reference panel provided    
     random.number <- stringi::stri_rand_strings(n=1, length=20, pattern = "[A-Za-z0-9]")
-    plink_input <- get_plink2_input_flag(bfile)
-    exit_status = system(paste0("plink2 ", plink_input, " --freq --make-just-bim --out ", random.number))
+
+    if(bfile_type=="plink1"){
+      exit_status = system(paste0("plink2 --bfile ", bfile, " --freq --make-just-bim --out ", random.number))
+    } else if(bfile_type=="plink2"){
+      exit_status = system(paste0("plink2 --bfile ", pfile, " --freq --make-just-pvar --out ", random.number))
+    }
 
     # Raise an error if the external command fails
     if (exit_status != 0) {
@@ -330,9 +325,14 @@ dataset.align <- function(dataset, bfile) {
     #   read_delim(paste0(random.number,".afreq")) %>% dplyr::select(REF, ALT, ALT_FREQS)
     # ))
     
-    # Read .bim and select/rename columns
-    bim <- read_delim(paste0(random.number, ".bim"), col_names = FALSE)
-    bim <- bim[, c(1, 4)]
+    # Read .pvar/.bim and select/rename columns
+    if(bfile_type=="plink1"){
+      bim <- read_delim(paste0(random.number, ".bim"), col_names = FALSE)
+      bim <- bim[, c(1, 4)]
+    } else if(bfile_type=="plink2"){
+      bim <- read_delim(paste0(random.number, ".bim"), col_names = TRUE)
+      bim <- bim[, c(1, 2)]
+    }
     names(bim) <- c("CHR", "BP")
     
     # Read .afreq and select needed columns
@@ -534,6 +534,7 @@ option_list <- list(
   make_option("--sdY", default=NULL, help="For a quantitative trait (type==quant), the population standard deviation of the trait. For quantitative traits, it can be a single value or a file containing per-phenotype `sdY` values. If not given, it will be estimated from beta and MAF"),
   make_option("--s", default=NULL, help="For a case control study (type==cc), the proportion of samples in dataset 1 that are cases"),
   make_option("--bfile", default=NULL, help="Path and prefix name of custom/default LD genotype files (PLINK bed/bim/fam or pgen/psam/pvar) - to compute effect allele frequency if missing"),
+  make_option("--bfile_type", default=NULL, help="Whether bfile is in plink2 pgen/pvar/psamj or plink1 bed/bim/fam format"),
   make_option("--grch", default=NULL, help="Genome reference build of GWAS sum stats"),
   make_option("--run_liftover", default=FALSE, type="logical", help="Set true to run liftover"),
   make_option("--maf", default=1e-04, help="MAF filter", metavar="character"),
@@ -631,7 +632,7 @@ if(as.numeric(opt$grch)==37 & isTRUE(opt$run_liftover)){
 
 # Align GWAS sum stats
 message("Align alleles to BIM file")
-dataset_munged <- dataset.align(dataset_munged, bfile=opt$bfile)
+dataset_munged <- dataset.align(dataset_munged, bfile=opt$bfile, bfile_type=opt$bfile_type)
 
 # Now that we have clean, aligned data, do a final check for duplicated CHR BP and remove all duplicates
 # Report the number of duplicates removed
